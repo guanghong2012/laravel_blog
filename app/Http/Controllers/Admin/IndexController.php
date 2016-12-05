ddabf0ac3d3e97bb1c4de466bccc5a9339b7c4f0
guanghong2012 <137084553@qq.com>
2016-12-05 09:42:30 +0800

    修改时区为上海

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -24,7 +25,7 @@ class IndexController extends Controller
         $statistics['software'] = $_SERVER["SERVER_SOFTWARE"];
         $statistics['sapi'] = php_sapi_name();
         $statistics['upload_max'] = ini_get('upload_max_filesize');
-        $statistics['time'] = date("Y年n月j日 H:i:s",time());
+        $statistics['time'] =  Carbon::createFromDate();
         $statistics['server_name'] = $_SERVER['SERVER_NAME'] . ' [ ' . gethostbyname($_SERVER['SERVER_NAME']) . ' ]';
         $statistics['space'] = round((disk_free_space(".") / (1024 * 1024)), 2) . 'M';
         return view('admin/Index/index',['title' => '后台首页','statistics' => $statistics]);
